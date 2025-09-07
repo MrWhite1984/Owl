fafc026fdb54636b23a3e942d3004771deb29afc
Артур Силкин <mrwhite19841@gmail.com>
2025-09-08 01:21:37 +0600

    First commit

---
+++ backend/src/main/java/ru/sibadi/confhub/SibadiConfhubApplication.java
@@ -0,0 +1,13 @@
+package ru.sibadi.confhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibadiConfhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibadiConfhubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/sibadi/confhub/SibadiConfhubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibadi.confhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibadiConfhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
