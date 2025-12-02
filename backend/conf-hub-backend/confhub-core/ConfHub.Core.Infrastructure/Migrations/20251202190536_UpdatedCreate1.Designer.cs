3bdc96f7a0847ed2ecf4fe6a989e49af07ff5e1c
Артур Силкин <mrwhite19841@gmail.com>
2025-12-03 01:08:10 +0600

    Applied migration

--- backend/conf-hub-backend/confhub-core/ConfHub.Core.Infrastructure/Migrations/20251202190536_UpdatedCreate1.Designer.cs
+++ backend/conf-hub-backend/confhub-core/ConfHub.Core.Infrastructure/Migrations/20251202190536_UpdatedCreate1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConfHub.Core.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConfHub.Core.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251202190536_UpdatedCreate1")]
+    partial class UpdatedCreate1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
