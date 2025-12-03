0d083ae5893e759d476dddfc156cc87784212404
Артур Силкин <mrwhite19841@gmail.com>
2025-12-03 18:34:17 +0600

    Create AddUser endpoint

--- backend/conf-hub-backend/confhub-core/ConfHub.Core.Application/Users/Services/UserService.cs
+++ backend/conf-hub-backend/confhub-core/ConfHub.Core.Application/Users/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using ConfHub.Core.Application.Common.Interfaces;
 using ConfHub.Core.Application.Users.Interfaces;
 using ConfHub.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace ConfHub.Core.Application.Users.Services
 {
@@ -18,8 +20,27 @@ namespace ConfHub.Core.Application.Users.Services
         public async Task AddAsync(Guid personId, string role, string passwordHash)
         {
             User user = new User(personId, role, passwordHash);
-            await _userRepository.AddAsync(user);
-            await _unitOfWork.SaveChangesAsync();
+            try
+            {
+                await _userRepository.AddAsync(user);
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                var postgresEx = ex.InnerException as PostgresException
+                      ?? ex.InnerException?.InnerException as PostgresException;
+
+                if (postgresEx != null && postgresEx.SqlState == PostgresErrorCodes.ForeignKeyViolation)
+                {
+                    if (postgresEx.ConstraintName?.Contains("person", StringComparison.OrdinalIgnoreCase) == true)
+                    {
+                        throw new ArgumentException($"Person с ID '{personId}' не существует.", nameof(personId));
+                    }
+
+                    throw new ArgumentException($"Указанный Person (ID: {personId}) не найден.", nameof(personId));
+                }
+                throw new InvalidOperationException("Ошибка при создании пользователя.", ex);
+            }
         }
 
         public async Task<IEnumerable<User>> GetUsersByPersonIdAsync(Guid id)
