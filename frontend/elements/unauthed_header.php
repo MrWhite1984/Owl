6e50e61561d3fa18c571087bc0a249b2f627646e
Артур Силкин <mrwhite19841@gmail.com>
2025-12-05 15:25:42 +0600

    Cleared everything from previous page files

--- frontend/elements/unauthed_header.php
+++
@@ -1,5 +0,0 @@
-<header class="row">
-    <div class="nav-btn-div col-1"><button id="nav-btn">&#9776;</button></div>
-    <div class="space-div col-8"></div>
-    <div class="login-link-div col-3"><a class="login-link" href="auth.php">Вход</a></div>
-</header>
